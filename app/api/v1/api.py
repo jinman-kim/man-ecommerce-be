376c8197d62dd8478516f1165c5e13c81c0ce011
kjm4027 <gameliker16@naver.com>
2024-10-12 14:59:13 +0900

    index endpoint 추가

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from api.v1.endpoints import users, items, auth, test
+from api.v1.endpoints import users, items, auth, test, index
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(test.router, prefix="/test", tags=["test data"])
+api_router.include_router(index.router, prefix="/index", tags=["index single item"])
--- app/main.py
+++ app/main.py
@@ -12,11 +12,17 @@ app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/o
 def on_startup():
     init_db(engine)
     # Elasticsearch 서비스 초기화 및 인덱스 생성
-    es_service = EsService()  # EsService 인스턴스 생성
-    es_service.create_index()  # 인덱스 생성
+    try:
+        es_service = EsService()  # EsService 인스턴스 생성
+        es_service.create_index()  # 인덱스 생성
+    except Exception as e:
+        print(f"Elasticsearch 서비스 초기화 중 오류 발생: {e}")
 
     # Kafka 서비스 초기화 및 토픽 생성
-    kafka_service = KafkaService()  # KafkaService 인스턴스 생성
-    kafka_service.create_topic("initial-topic")  # 초기 토픽 생성
-
+    try:
+        kafka_service = KafkaService()  # KafkaService 인스턴스 생성
+        kafka_service.create_topic("initial-topic")  # 초기 토픽 생성
+    except Exception as e:
+        print(f"Kafka 서비스 초기화 중 오류 발생: {e}")
+        
 app.include_router(api_router, prefix=settings.API_V1_STR)
